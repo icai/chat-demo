c081d5c2eb924be74149e13be40ea458cddd812d
Terry Cai <gidcai@gmail.com>
2014-01-30 17:02:16 +0800

    tidy code

--- server.js
+++ server.js
@@ -1,59 +1,76 @@
 
 var express = require('express'),
     app = express()
-    serv = require('http').createServer(app),
-    io = require('socket.io').listen(serv),
+    http = require('http'),
+    socketio = require('socket.io'),
+    path = require('path'),
     logger = require('winston'),
     program = require('commander');
 
-
-    
-//  
 // SETUP
-//
 
 logger.cli();
 logger.default.transports.console.timestamp = true;
-    
+
 program.version('0.1')
     .option('-p, --port [num]', 'Set the server port (default 8080)')
     .option('-H, --disableheartbeats', 'Disable heartbeats')
     .parse(process.argv)
-    
-var server = "localhost";
-if(program.args.length==1) {
-    server = program.args[0];
-} else if (program.args.length==0) {
-    logger.warn("Defaulting to localhost.");
-}
-
-var port = 3000;
-if(program.port) {
-    logger.info("Setting port to " + program.port);
-    port = program.port;
-}
-//app.listen(port);
-
-serv.listen(port); // socket io
 
 
-//app.server.listen(port);
+/**
+ * Configuration
+ */
 
-if(program.disableheartbeats) {
-    io.set("heartbeats", false)
-}
-
-io.set("log level", 0);
-
-//
-// LISTENERS
-//
-
-app.use('/', express.static(__dirname + '/publish'));
-app.get('/', function (req, res) {
-  res.sendfile('index.html');
+app.configure(function(){
+    app.set('port', process.env.PORT || program.port || 3000);
+    logger.info("Setting port to " + app.get('port'));
+    app.use(express.favicon());
+    app.use(express.logger('short'));
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+    
+
+
+
+/**
+ * Routes
+ */
+
+
+app.get('/', function (req, res) {
+  res.sendfile('publish/index.html');
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port "+ app.get('port') +" in "+ app.get('env') +" mode.");
+});
+
+
+
+ /**
+  * Socket IO
+  * @type {[type]}
+  */
+var io = socketio.listen(server);
+
+io.configure(function() {
+	if(program.disableheartbeats) {
+	    io.set("heartbeats", false)
+	}
+    io.enable('browser client minification');
+    io.set("log level", 0);
+});
 
 io.sockets.on('connection', function(socket) {
     socket.on('chat', function(data) {
